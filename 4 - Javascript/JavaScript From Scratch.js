2a04adf44cd0491c6a081c0b329f0d170d26992b
Hossein <hossein.dehghanipour1998@gmail.com>
2020-03-27 04:45:18 +0430

    Session 0
    An Introduction of what Javascript is.

---
+++ 4 - Javascript/JavaScript From Scratch.js
@@ -0,0 +1,11 @@
+/* 
+
+====== INTRODUCTION =========
+# Javascript doesn't need any professional IDE. All we need is a text editor and a web browser like google chrome or etc.
+# JS is a non-typed language which means there is no need to define a type (int , string, ... ) before the variable name.
+# JS is also a "dynamic programming language" which means a variable can be defined as a String but later in the code be assigned an integer number ( like python)
+# Suggested Text Editors : Atom , VS Code , Sublime Text , Notepad++
+# Suggested IDEs : WebStorm , Eclipse , Visual Stduio
+
+*/
+
