f39112127f904f05c80c0ed53f9de21df12abc6d
Hossein <hossein.dehghanipour1998@gmail.com>
2020-03-29 03:04:20 +0430

    Event
    Event Listeners Added.
    There is a bug in the TExtField.add() function.

--- 4 - Javascript/3 - DOM/script.js
+++ 4 - Javascript/3 - DOM/script.js
@@ -1,3 +1,4 @@
+/*
 document.getElementsByTagName("second") ;
 // Access an HTML element by it's class name.
 // we say elements by tagName because there are multiple elements named by a class.
@@ -21,4 +22,39 @@ h1.className = "coolTitle" ;
 // search for codePen.io for cool themes.
 
 
-// =============== EVENTS ==============
+// =============== EVENTS ====================
+
+var btn = document.getElementsByTagName("button")[1]; // we say the first button on the page by [0]
+function saveTheName(){
+btn.addEventListener("click",function() 
+	{ console.log("Clicked The Button");
+})
+*/
+// *******************
+var textField = document.getElementById("inputName");
+var unorderedList = document.querySelector("ul");
+var enterBtn = document.getElementById("enterBtn");
+
+function saveTheItem(){
+	var string = textField.value ;
+	if ( string.length > 0 ){
+		var li = document.createElement("li");
+		var node = document.createTextNode(string) ;
+		li.appendChild(node);
+		unorderedList.appendChild(li);
+		textField.value = "" ;
+	}
+	else{
+		alert("Enter Something to submit!");
+	}
+}
+
+
+enterBtn.addEventListener("click",saveTheItem);
+
+textField.addEventListener("keypress",function(event){
+	if(event.keyCode === 13 ){
+		saveTheItem();
+	}
+}
+); // When we press enter button on the keyboard this happens
