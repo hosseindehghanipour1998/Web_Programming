951c37087a75d7e6e639078e76a6e9b07799cab5
Hossein <hossein.dehghanipour1998@gmail.com>
2020-04-22 16:17:50 +0430

    Callback Hell
    Making the jquery code cleaner and bug free.

--- 6 - Async JS/2 - Callback Hell/async.js
+++ 6 - Async JS/2 - Callback Hell/async.js
@@ -1,4 +1,7 @@
+// Messy Solution
+/*
 window.onload = function() {
+	function handleError(){console.log(error);}
 	$.ajax(
 		{
 			type:"GET",
@@ -21,21 +24,68 @@ window.onload = function() {
 									success:function(data){
 										console.log(data)
 									},
-									error:function(jqXHR,textStatus,error){
-										console.log(error);
-									}
+									error:handleError
 								}
 							);
 						},
-						error:function(jqXHR,textStatus,error){
-							console.log(error);
+						error:handleError
 						}
 					}
 				);
 			},
-			error:function(jqXHR,textStatus,error){
-				console.log(error);
+			error:handleError
 			}
 		}
 	);
 }
+*/
+
+//Cleaner Solution :
+window.onload = function() {
+
+	function handleError(jqXHR , textStatus ,error){console.log(error);}
+
+	$.ajax(
+		{
+			type:"GET",
+			url:"data/tweets.json",
+			// Success means what do i do if we were successfuk in retrieving the data.
+			success:callbackTweets,
+			error:handleError
+		}
+	);
+
+	function callbackTweets(data){
+		console.log(data)
+		$.ajax(
+			{
+				type:"GET",
+				url:"data/friends.json",
+				// Success means what do i do if we were successfuk in retrieving the data.
+				success:callbackFriends,
+				error:handleError
+
+			}
+		);
+	}
+
+	function callbackFriends(data){
+		console.log(data)
+		$.ajax(
+			{
+				type:"GET",
+				url:"data/videos.json",
+				// Success means what do i do if we were successfuk in retrieving the data.
+				success:function(data){
+					console.log(data)
+				},
+				error:handleError
+			}
+		);
+	}
+}
+
+/*
+	As you can see , handling a Async Call with JQuery is a pain in the back. So we'd better create promises using JS Promises.
+
+*/
