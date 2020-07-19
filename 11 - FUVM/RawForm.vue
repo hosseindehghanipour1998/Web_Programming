3fd91b17be438309137334abc119c74c115f9730
Hossein <hossein.dehghanipour1998@gmail.com>
2020-07-19 13:48:27 +0430

    Create RawForm.vue

---
+++ 11 - FUVM/RawForm.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+	// Imports
+	//import formOne from './components/formOne.vue';
+	//import formTwo from './components/formTwo.vue';
+
+	export default {
+
+		props : {
+		
+		}
+		
+		components: {
+			//'form-one': formOne,
+			//'form-two': formTwo
+		},
+		
+		data () {
+			return {
+				//component: 'form-one'
+			}
+		},
+		
+		methods: {
+
+		},
+		
+		
+	}
+</script>
+
+ 
+<style>
+
+</style>
